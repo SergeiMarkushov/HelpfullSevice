8b567511034837457311be8dd2899c024f371744
SeregaMarkush <s.markushov@icloud.com>
2023-10-02 21:02:47 +0300

    Initial commit

---
+++ src/main/java/ru/niseco/helpfullservice/HelpfullSeviceApplication.java
@@ -0,0 +1,13 @@
+package ru.niseco.helpfullservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpfullSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpfullSeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/niseco/helpfullservice/HelpfullSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.niseco.helpfullservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpfullSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
